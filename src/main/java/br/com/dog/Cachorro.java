ab89bbb6e14413057e176c8b053a5df1153a6892
ELAINE PAZ <elaine.apaz@telefonicabigdata.onmicrosoft.com>
2025-06-10 09:16:35 -0300

    atributos

---
+++ src/main/java/br/com/dog/Cachorro.java
@@ -0,0 +1,14 @@
+package br.com.dog;
+
+public class Cachorro {
+    private String decricao;
+    private String energia;
+    private String obdiencia;
+    private String inteligencia;
+    private String territorialiasta;
+    private String apegoAoDono;
+    private String tendenciaALatir;
+    private String amizadeComCrianças;
+    private String amizadeComOutrosAnimais;
+
+}
