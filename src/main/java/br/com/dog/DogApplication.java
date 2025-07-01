ef1eeba5f766be8faba9bdeb49ce2d23347680a3
rolivsan <rolivsan@gmail.com>
2025-07-01 10:13:58 -0300

    up code

--- src/main/java/br/com/dog/DogApplication.java
+++ src/main/java/br/com/dog/DogApplication.java
@@ -1,13 +1,30 @@
 package br.com.dog;
 
+import br.com.dog.dto.CachorroDTORequest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class DogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DogApplication.class, args);
+	}
+
+
+	@Bean
+	public List<CachorroDTORequest> cachorroList(){
+		List<CachorroDTORequest> cachorroList = new ArrayList<>();
+		cachorroList.add(new CachorroDTORequest(1L,"Rex", "Labrador", 5));
+		cachorroList.add(new CachorroDTORequest(2L,"Fido", "Beagle", 3));
+		cachorroList.add(new CachorroDTORequest(3L,"Buddy", "Golden Retriever", 4));
+
+		return cachorroList;
 	}
 
 }
---
+++ src/main/java/br/com/dog/Main.java
@@ -0,0 +1,10 @@
+package br.com.dog;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("Elaine", 30);
+        Pessoa p2 = new Pessoa("Pedro", 20);
+        System.out.println(p1);
+        System.out.println(p2);
+    }
+}
